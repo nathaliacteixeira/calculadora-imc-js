027588cce43e00c290a2443ddc655260746d22e6
Nathalia Cardoso <teixeira.nathalia.c@gmail.com>
2021-09-14 20:08:42 -0300

    alterações

--- script.js
+++ script.js
@@ -8,92 +8,52 @@ function calcularIMC(){
         return;
     }
 
-    if(altura!=0 || peso != 0){
-        imc = (peso / (altura * altura));
-    }else{
+    if(altura < 0 || peso < 0){
         alert("Altura e/ou peso precisam ser maior que 0!");
+        return;
     }
+
+    imc = peso / Math.pow(altura, 2);
      
     document.querySelector('.imc').innerHTML = "O seu IMC é: " + imc.toFixed(2); 
 
-    let classeAntes = document.querySelector('.classificacao').className;
-    let classeDepois = document.querySelector('.classificacao');
-
     if(imc < 16) {
-
-        if(classeAntes != ''){
-            classeDepois.classList.remove(classeAntes);
-        }
-
-        classeDepois.classList.add('magreza-grave');
+        classeClassificacao = ('magreza-grave');
         classificacao = 'Magreza grave';
 
     }else if(imc >= 16 && imc < 17) {
-
-        if(classeAntes != ''){
-            classeDepois.classList.remove(classeAntes);
-        }
-
-        classeDepois.classList.add('magreza-moderada');
+        classeClassificacao = ('magreza-moderada');
         classificacao = 'Magreza moderada';
 
     }else if(imc >= 17 && imc < 18.5) {
-
-        if(classeAntes != ''){
-            classeDepois.classList.remove(classeAntes);
-        }
-
-        classeDepois.classList.add('magreza-leve');
+        classeClassificacao = ('magreza-leve');
         classificacao = 'Magreza leve';
 
     }else if(imc >= 18.5 && imc < 25) {
-
-        if(classeAntes != ''){
-            classeDepois.classList.remove(classeAntes);
-        }
-
-        classeDepois.classList.add('saudavel');
+        classeClassificacao = ('saudavel');
         classificacao = 'Saudável';
 
     }else if(imc >= 25 && imc < 30) {
-
-        if(classeAntes != ''){
-            classeDepois.classList.remove(classeAntes);
-        }
-
-        classeDepois.classList.add('sobrepeso');
+        classeClassificacao = ('sobrepeso');
         classificacao = 'Sobrepeso';
 
     }else if(imc >= 30 && imc < 35) {
-
-        if(classeAntes != ''){
-            classeDepois.classList.remove(classeAntes);
-        }
-
-        classeDepois.classList.add('obesidade-grau-i');
+        classeClassificacao = ('obesidade-grau-i');
         classificacao = 'Obesidade Grau I';
 
     }else if(imc >= 35 && imc < 40) {
-
-        if(classeAntes != ''){
-            classeDepois.classList.remove(classeAntes);
-        }
-
-        classeDepois.classList.add('obesidade-grau-ii');
+        classeClassificacao = ('obesidade-grau-ii');
         classificacao = 'Obesidade Grau II (severa)';
         
     }else if(imc >= 40) {
-
-        if(classeAntes != ''){
-            classeDepois.classList.remove(classeAntes);
-        }
-
-        classeDepois.classList.add('obesidade-grau-iii');
+        classeClassificacao = ('obesidade-grau-iii');
         classificacao = 'Obesidade Grau III (mórbida)';
 
     }
 
-    classeDepois.innerText = 'A classificação do seu IMC é: ' + classificacao;
+    let pClassificacao =  document.querySelector('.classificacao');
+    pClassificacao.innerText = 'A classificação do seu IMC é: ' + classificacao;
+    pClassificacao.className(classeClassificacao);
 }
 
 function limpar(){
